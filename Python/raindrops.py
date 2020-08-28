8c50364b3f333eba0def8d2abbaf4926fdffde59
Orian Shkrobut <shkrobut.orian@gmail.com>
2020-08-28 08:48:03 -0500

    [Solution] Raindrops (third iteration)

--- Python/raindrops.py
+++ Python/raindrops.py
@@ -2,9 +2,12 @@ def convert(number):
     result = ""
     if number % 3 == 0:
         result += "Pling"
+        return result
     if number % 5 == 0:
         result += "Plang"
+        return result
     if number % 7 == 0:
         result += "Plong"
+        return result
     else:
         return str(number)
